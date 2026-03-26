261d8ebd23542b87130f56d8ea642eed010307e6
SiimNu89 <siimnurja@gmail.com>
2026-03-26 08:55:28 +0200

    updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,19 +29,15 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        
-        @vite(['resources/js/app.ts'])
-
-        
+        @routes
+        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
