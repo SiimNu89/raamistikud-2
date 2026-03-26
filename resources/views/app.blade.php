de91745ce66c8cedd6e6844a5361fd164bf969af
SiimNu89 <siimnurja@gmail.com>
2026-03-26 11:14:06 +0200

    updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,7 +37,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
-        @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.ts'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
